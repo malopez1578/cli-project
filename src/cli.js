9b3fa9eb23c8d54992827262127a7ec09fe0a610
malopez1578 <malopez1578@gmail.com>
2020-04-12 13:28:56 -0500

    Update(cli.js, create-project): se agrega los parametos basicos del proyecto

--- src/cli.js
+++ src/cli.js
@@ -1,3 +1,75 @@
-export function cli(options) {
-  console.log(`cli -> options`, options);
+import arg from 'arg';
+import inquirer from 'inquirer';
+
+function parseArguments(rawArgs) {
+  const args = arg(
+    {
+      '--new': Boolean,
+      '--start': Boolean,
+      '--init': Boolean,
+      '--install': Boolean,
+      '--git': Boolean,
+      '-g': '--git',
+      '-ins': '--install',
+      '-i': '--init',
+      '-n': '--new',
+      '-s': '--start',
+    },
+    {
+      argv: rawArgs.slice(2),
+    }
+  );
+  return {
+    new: args['--new'] || false,
+    start: args['--start'] || false,
+    init: args['--init'] || false,
+    install: args['--install'] || false,
+    git: args['--git'] || false,
+    template: args._[0],
+  };
+}
+
+async function promptMissingOptions(options) {
+  const defaultTemplate = 'Typescript';
+  if (options.skipPrompts) {
+    return {
+      ...options,
+      template: options.template || defaultTemplate,
+    };
+  }
+
+  const questions = [];
+  if (options.new) {
+    if (!options.template) {
+      questions.push({
+        type: 'list',
+        name: 'template',
+        message: 'please choose which game template to use',
+        choices: ['Javascript', 'Typescript'],
+        default: defaultTemplate,
+      });
+    }
+  }
+  if (options.init) {
+    if (!options.git) {
+      questions.push({
+        type: 'confirm',
+        name: 'git',
+        message: 'initialze a git repository',
+        default: false,
+      });
+      options.template = 'webpackInit';
+    }
+  }
+  const answers = await inquirer.prompt(questions);
+  return {
+    ...options,
+    template: options.template || answers.template,
+    git: options.git || answers.git || false,
+  };
+}
+
+export async function cli(args) {
+  let options = parseArguments(args);
+  options = await promptMissingOptions(options);
 }
